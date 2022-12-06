2b81bc43a4717f0c3a969728d615fbf6015e5a6a
User <User@DESKTOP-ESP2JT1>
2022-12-06 16:25:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int freeSpace = width * lenght * height;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Done")
+                {
+                    Console.WriteLine($"{freeSpace} Cubic meters left.");
+                    break;
+                }
+
+                int spaceToTake = int.Parse(command);
+                if (spaceToTake > freeSpace)
+                {
+                    Console.WriteLine($"No more free space! You need {spaceToTake - freeSpace} Cubic meters more.");
+                    break;
+                }
+
+                freeSpace -= spaceToTake;
+                if (freeSpace == 0)
+                {
+                    Console.WriteLine($"0 Cubic meters left.");
+                    break;
+
+                }
+            }
+        }
+    }
+}
